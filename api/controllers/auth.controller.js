4d618957fadb490369eb0a6c6c331d1de8b46605
kmerchant1 <kaiden.merchant@gmail.com>
2024-07-14 13:54:38 -0400

    custom error handler (simple passwd, etc.)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcyrptjs from 'bcryptjs';
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
     const {username, password, email} = req.body;
     const hash = bcyrptjs.hashSync(password, 10);
     const newUser = new User({username, password:hash, email});
@@ -8,7 +9,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json("User created successfully");
     }catch (error){
-        res.status(500).json(error.message)
+        next(error);
     }
     
 };
