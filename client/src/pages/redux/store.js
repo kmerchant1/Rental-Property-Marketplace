df5a1d5bdc2faa095978de086aae89175212d676
kmerchant1 <kaiden.merchant@gmail.com>
2024-07-20 12:28:27 -0400

    redux persist so we can have access to the current user info throughout the session and across pages.

--- client/src/pages/redux/store.js
+++ client/src/pages/redux/store.js
@@ -1,8 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { presistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({userReducer});
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+const presistedReducer = presistReducer({persistConfig, rootReducer})
+
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: presistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
+
+export const persistor = persistStore(store);
